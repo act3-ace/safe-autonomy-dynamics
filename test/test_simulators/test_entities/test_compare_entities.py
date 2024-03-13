61795e54620d6ef1e0e05ccdc7f1b2c5f0800b73
Aditesh Kumar <akumar@toyon.com>
2024-03-12 17:57:21 -0700

    misc: unit tests for entity relative pos/vel

--- test/test_simulators/test_entities/test_compare_entities.py
+++ test/test_simulators/test_entities/test_compare_entities.py
@@ -9,9 +9,9 @@
 # limitation or restriction. See accompanying README and LICENSE for details.
 # -------------------------------------------------------------------------------
 
-This module defines tests for the CWHRotationSpacecraft entity.
+This module defines tests of equality between different entities.
 
-Author: John McCarroll, Andy Barth
+Author: Aditesh Kumar
 """
 
 import importlib
@@ -24,10 +24,9 @@ from test.conftest import read_test_cases, delimiter
 
 # Define test assay
 test_cases_file_path = os.path.join(os.path.split(__file__)[0], "../../test_cases/compare_entities_test_cases.yaml")
-parameterized_fixture_keywords = ["this_entity",
-                                  "other_entity",
-                                  "expect_equal"]
+parameterized_fixture_keywords = ["this_entity", "other_entity", "expect_equal"]
 test_configs, IDs = read_test_cases(test_cases_file_path, parameterized_fixture_keywords)
+
 
 def build_entity(entity_name, entity_config):
     mod_name, _, obj_name = entity_config['entity_class'].rpartition('.')
@@ -40,19 +39,22 @@ def build_entity(entity_name, entity_config):
         entity.state = np.array(initial_state, dtype=entity.state.dtype)
     return entity
 
+
 @pytest.fixture
 def this_entity(request):
     return build_entity('this', request.param)
+
 
 @pytest.fixture
 def other_entity(request):
     return build_entity('other', request.param)
 
+
 @pytest.fixture
 def expect_equal(request):
     return request.param
 
+
 @pytest.mark.parametrize(delimiter.join(parameterized_fixture_keywords), test_configs, indirect=True, ids=IDs)
 def test_compare_entities(this_entity, other_entity, expect_equal):
     assert (this_entity == other_entity) == expect_equal
-
